7865a211a2c5756a80622f4f34f79769116d2d05
945219317 <945219317@qq.com>
2019-07-14 11:01:34 +0800

    RBTree的删除元素

--- DataStructures/src/tree/red_black_tree/RBTree.java
+++ DataStructures/src/tree/red_black_tree/RBTree.java
@@ -1,30 +1,33 @@
 package tree.red_black_tree;
 
-import map.Map;
 import set.FileOperation;
 
 import java.util.ArrayList;
 
 /**
- *  1.每个节点是红色或黑色
- *  2.根节点是黑色
- *  3.每个叶子节点(最后的空节点)是黑色
- *  4.如果一个节点是红色，其孩子节点必定为黑色
- *  5.从任意节点到其所有的叶子节点中途经过的黑色节点个数一样
+ * 1.每个节点是红色或黑色
+ * 2.根节点是黑色
+ * 3.每个叶子节点(最后的空节点)是黑色
+ * 4.如果一个节点是红色，其孩子节点必定为黑色
+ * 5.从任意节点到其所有的叶子节点中途经过的黑色节点个数一样
  *
- *  红黑树实现了黑节点的绝对平衡，而非标准意义的平衡
- *  单个操作的最坏的时间复杂度是2logN，即所有节点均为"3-"节点
- *  该实现为红色节点左倾的红黑树
+ * 红黑树实现了黑节点的绝对平衡，而非标准意义的平衡
+ * 单个操作的最坏的时间复杂度是2logN，即所有节点均为"3-"节点
+ * 该实现为红色节点左倾的红黑树,即不存在任何一个节点的右节点是红色节点
+ *
+ * @author VULCAN
  */
-public class RBTree<K extends Comparable<K>, V>{
+@SuppressWarnings({"WeakerAccess", "unused"})
+public class RBTree<K extends Comparable<K>, V> {
 
     private static final boolean RED = true;
     private static final boolean BLACK = false;
 
+    @SuppressWarnings("WeakerAccess")
     private class Node {
         public K key;
         public V value;
-        public Node left,right;
+        public Node left, right;
         public boolean color;
 
         public Node(K key, V value) {
@@ -37,7 +40,7 @@ public class RBTree<K extends Comparable<K>, V>{
 
         @Override
         public String toString() {
-            return this.key.toString() + ":" + this.value.toString();
+            return this.key + ":" + this.value + "  " + (this.color ? "RED" : "BLACK");
         }
     }
 
@@ -54,13 +57,13 @@ public class RBTree<K extends Comparable<K>, V>{
     }
 
     private boolean isRed(Node node) {
-        if(node == null){
+        if (node == null) {
             return BLACK;
         }
         return node.color;
     }
 
-    private Node leftRotate(Node node){
+    private Node leftRotate(Node node) {
         Node x = node.right;
         node.right = x.left;
         x.left = node;
@@ -69,7 +72,7 @@ public class RBTree<K extends Comparable<K>, V>{
         return x;
     }
 
-    private Node rightRotate(Node node){
+    private Node rightRotate(Node node) {
         Node x = node.left;
         node.left = x.right;
         x.right = node;
@@ -78,9 +81,10 @@ public class RBTree<K extends Comparable<K>, V>{
         return x;
     }
 
-    private void flipColor(Node node){
-        node.color = RED;
-        node.left.color = node.right.color = BLACK;
+    private void flipColor(Node node) {
+        node.color = !node.color;
+        node.left.color = !node.left.color;
+        node.right.color = !node.right.color;
     }
 
     private Node add(Node node, K key, V value) {
@@ -95,12 +99,12 @@ public class RBTree<K extends Comparable<K>, V>{
         } else {
             node.value = value;
         }
-        if(isRed(node.right) && !isRed(node.left)){
+        if (isRed(node.right) && !isRed(node.left)) {
             node = leftRotate(node);
-        }else if(isRed(node.left) && isRed(node.left.left)){
+        } else if (isRed(node.left) && isRed(node.left.left)) {
             node = rightRotate(node);
-        }
-        if(isRed(node.right) && isRed(node.left)){
+            flipColor(node);
+        } else if (isRed(node.right) && isRed(node.left)) {
             flipColor(node);
         }
         return node;
@@ -113,57 +117,137 @@ public class RBTree<K extends Comparable<K>, V>{
         return node;
     }
 
-    private Node removeMin(Node node) {
-        if (node.left != null) {
-            node.left = removeMin(node.left);
-            return node;
+    private Node maximum(Node node) {
+        if (node.right != null) {
+            return maximum(node.right);
         }
-        Node ret = node.right;
-        node.right = null;
-        size--;
-        return ret;
+        return node;
+    }
+
+    public void deleteMin() {
+        if (root != null) {
+            root = deleteMin(root);
+            if (root != null) {
+                root.color = BLACK;
+            }
+        }
+    }
+
+    private Node deleteMin(Node h) {
+        if (h.left == null) {
+            size--;
+            return null;
+        }
+        if (!isRed(h.left) && !isRed(h.left.left)) {
+            h = moveRedLeft(h);
+        }
+        h.left = deleteMin(h.left);
+        return fixUp(h);
+    }
+
+    public void deleteMax() {
+        if (root != null) {
+            root = deleteMax(root);
+            if (root != null) {
+                root.color = BLACK;
+            }
+        }
+    }
+
+    private Node deleteMax(Node h) {
+        if (isRed(h.left)) {
+            h = rightRotate(h);
+        } else if (h.right == null) {
+            size--;
+            return null;
+        } else if (!isRed(h.right) && !isRed(h.right.left)) {
+            h = moveRedRight(h);
+        }
+        h.right = deleteMax(h.right);
+        return fixUp(h);
+    }
+
+    private Node moveRedLeft(Node h) {
+        flipColor(h);
+        //这种情况对于之后的fixUp如果不进行预处理是有问题的
+        if (isRed(h.right.left)) {
+            h.right = rightRotate(h.right);
+            h = leftRotate(h);
+            flipColor(h);
+        }
+        return h;
+    }
+
+    private Node moveRedRight(Node h) {
+        flipColor(h);
+        //这种情况对于之后的fixUp如果不进行预处理貌似是没有问题的
+//        if (isRed(h.left.left)){
+//            h = rightRotate(h);
+//            flipColor(h);
+//        }
+        return h;
+    }
+
+    private Node fixUp(Node h) {
+        if (isRed(h.right)) {
+            h = leftRotate(h);
+        }
+        if (isRed(h.left) && isRed(h.left.left)) {
+            h = rightRotate(h);
+        }
+        if (isRed(h.left) && isRed(h.right)) {
+            flipColor(h);
+        }
+        return h;
     }
 
     public V remove(K key) {
         Node ret = getNode(root, key);
         if (ret != null) {
-            root = remove(root, key);
+            root = delete(root, key);
+            if (root != null) {
+                root.color = BLACK;
+            }
             return ret.value;
         }
         return null;
     }
 
+    /**
+     * 不论颜色，任意节点的子节点不可能存在单边黑色，因为要满足黑平衡
+     * 1.删除元素为叶子节点，且红色直接删除
+     * 2.删除元素为非叶子节点，且为黑色，同时该节点只有红色左节点，则红色赋值给该节点后删除红色节点
+     * 3.删除元素为非叶子节点，且两边都有子节点，则删除后继节点，后继节点为红色直接删除
+     *
+     * @param node
+     * @param key
+     * @return
+     */
     private Node remove(Node node, K key) {
-        if (node != null) {
-            if (key.compareTo(node.key) < 0) {
-                node.left = remove(node.left, key);
-            } else if (key.compareTo(node.key) > 0) {
-                node.right = remove(node.right, key);
+        if (key.compareTo(node.key) < 0) {
+            if (!isRed(node.left) && !isRed(node.left.left)) {
+                node = moveRedLeft(node);
+            }
+            node.left = remove(node.left, key);
+        } else {
+            if (isRed(node.left)) {
+                node = rightRotate(node);
+            } else if (key.compareTo(node.key) == 0 && (node.right == null)) {
+                size--;
+                return null;
+            } else if (!isRed(node.right) && !isRed(node.right.left)) {
+                node = moveRedRight(node);
+            }
+            if (key.compareTo(node.key) == 0) {
+                Node minimum = minimum(node.right);
+                node.key = minimum.key;
+                node.value = minimum.value;
+                node.right = deleteMin(node.right);
             } else {
-                Node ret;
-                if (node.left == null && node.right == null) {
-                    size--;
-                    return null;
-                } else if (node.left != null && node.right == null) {
-                    ret = node.left;
-                    node.left = null;
-                    size--;
-                    return ret;
-                } else if (node.left == null && node.right != null) {
-                    ret = node.right;
-                    node.right = null;
-                    size--;
-                    return ret;
-                } else {
-                    Node min = minimum(node.right);
-                    node.key = min.key;
-                    node.value = min.value;
-                    node.right = removeMin(node.right);
-                    return node;
-                }
+                node.right = remove(node.right, key);
             }
         }
-        return null;
+        return fixUp(node);
     }
 
     public boolean contains(K key) {
@@ -205,28 +289,69 @@ public class RBTree<K extends Comparable<K>, V>{
     }
 
     private boolean isRB(Node node) {
-        if(node == null) {
+        if (node == null) {
             return true;
         }
-        if(node.color == RED && (isRed(node.left) || isRed(node.right))) {
+        if ((node.color == RED && (isRed(node.left) || isRed(node.right))) || isRed(node.right)) {
             return false;
         }
         return isRB(node.left) && isRB(node.right);
     }
 
-    public boolean isRB(){
-        if(root.color != BLACK){
-            return false;
+    public boolean isRB() {
+        if (root != null) {
+            if (root.color != BLACK) {
+                return false;
+            }
+            return isRB(root);
         }
-        return isRB(root);
+        return true;
     }
 
-    public static void main(String[] args){
+    private Node delete(Node h, K key) {
+        int cmp = key.compareTo(h.key);
+        if (cmp < 0) {
+            if (!isRed(h.left) && !isRed(h.left.left)) {
+                h = moveRedLeft(h);
+            }
+            h.left = delete(h.left, key);
+        } else if (cmp > 0) {
+            if (isRed(h.left)) {
+                h = rightRotate(h);
+            } else if ((!isRed(h.right) && !isRed(h.right.left))) {
+                h = moveRedRight(h);
+            }
+            h.right = delete(h.right, key);
+        } else {
+            if (isRed(h.left)) {
+                h = rightRotate(h);
+            } else if (h.right == null) {
+                size--;
+                return null;
+            } else if ((!isRed(h.right) && !isRed(h.right.left))) {
+//                if(!isRed(h) && h != root){                   //只有一种情况，h为root节点时h才会是黑色，其余都是红色
+//                    System.out.println(111);
+//                }
+                h = moveRedRight(h);
+            }
+            if(key.compareTo(h.key) == 0){
+                Node minimum = minimum(h.right);
+                h.key = minimum.key;
+                h.value = minimum.value;
+                h.right = deleteMin(h.right);
+            } else {
+                h.right = delete(h.right, key);
+            }
+        }
+        return fixUp(h);
+    }
+
+    public static void main(String[] args) {
 
         System.out.println("The Decameron Giovanni Boccaccio");
 
         ArrayList<String> words = new ArrayList<>();
-        if(FileOperation.readFile("The-Decameron-Giovanni-Boccaccio.txt", words)) {
+        if (FileOperation.readFile("The-Decameron-Giovanni-Boccaccio.txt", words)) {
             System.out.println("Total words: " + words.size());
 
             RBTree<String, Integer> map = new RBTree<>();
@@ -235,6 +360,10 @@ public class RBTree<K extends Comparable<K>, V>{
                     map.set(word, map.get(word) + 1);
                 } else {
                     map.put(word, 1);
+                    if (!map.isRB()) {
+                        throw new RuntimeException("Error");
+                    }
+
                 }
             }
 
@@ -242,8 +371,51 @@ public class RBTree<K extends Comparable<K>, V>{
             System.out.println("Frequency of PRIDE: " + map.get("pride"));
             System.out.println("Frequency of PREJUDICE: " + map.get("prejudice"));
             System.out.println("isRBTree: " + map.isRB());
+            RBTree.Node minimum = map.minimum(map.root);
+            System.out.println("min:" + minimum.key);
+            map.deleteMin();
+            System.out.println("min:" + map.minimum(map.root).key);
+            System.out.println("isRBTree: " + map.isRB());
+            RBTree.Node maximum = map.maximum(map.root);
+            System.out.println("max:" + maximum.key);
+            map.deleteMax();
+            System.out.println("max:" + map.maximum(map.root).key);
+            System.out.println("isRBTree: " + map.isRB());
+//            int i1 = map.size / 2;
+//            for (int i = 0; i < i1; i++) {
+//                map.deleteMin();
+//                if (!map.isRB()) {
+//                    throw new RuntimeException("error");
+//                }
+//                map.deleteMax();
+//                if (!map.isRB()) {
+//                    throw new RuntimeException("error");
+//                }
+//            }
+//            System.out.println(map.size);
+            for (String word : words) {
+                map.remove(word);
+                if (!map.isRB()) {
+                    throw new RuntimeException("error");
+                }
+            }
+            System.out.println("size : " + map.getSize());
         }
-        System.out.println();
+
+
+        RBTree<Integer, Object> map = new RBTree<>();
+        for (int i = 0; i < 40; i++) {
+            map.put(i, null);
+        }
+        for (int i = 0; i < 20; i++) {
+            map.deleteMin();
+        }
+        System.out.println(map.isRB());
+        for (int i = 0; i < 20; i++) {
+            map.deleteMax();
+        }
+        System.out.println(map.isRB());
+        System.out.println(map.size);
     }
 
 }
